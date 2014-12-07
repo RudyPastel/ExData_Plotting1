f28ba83c47c0454ec2f1ec741b2646373315c1ee
RudyPastel <rudy.pastel@gmail.com>
2014-12-07 01:52:24 +0100

    Create plot2.R
    R code for the second plot. Data should be in the same folder.

---
+++ plot2.R
@@ -0,0 +1,31 @@
+rm(list=ls())
+#Load and format the data
+info = read.table(file = "household_power_consumption.txt",
+                  header = FALSE,
+                  quote = "",
+                  sep = ";",
+                  na.strings = "?",
+                  skip = 66637,
+                  nrow = 2880,
+                  col.names = c("Date",
+                                "Time",
+                                "Global_active_power",
+                                "Global_reactive_power",
+                                "Voltage",
+                                "Global_intensity",
+                                "Sub_metering_1",
+                                "Sub_metering_2",
+                                "Sub_metering_3"),
+                  stringsAsFactors = FALSE
+)
+info$Date = as.Date(info$Date,format = "%d/%m/%Y")
+info$Time = strptime(paste(info$Date,info$Time),format = "%Y-%m-%d %H:%M:%S")
+
+# Plot to screen
+with(info,plot(Time,Global_active_power, col = "black",type = "l",xlab = "",  ylab = "Global Active Power (kilowatts)"))
+
+
+# Plot to a png file
+png("plot2.png")
+with(info,plot(Time,Global_active_power, col = "black",type = "l",xlab = "",  ylab = "Global Active Power (kilowatts)"))
+dev.off()
